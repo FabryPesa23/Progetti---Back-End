4dded409927b2eb0e24d4eed1ed52f4206d2a47d
Fabrizio Pesaresi <fabryzio2391@gmail.com>
2026-03-13 13:22:07 +0100

    test main

--- Progetto U1 S1/src/Main.java
+++ Progetto U1 S1/src/Main.java
@@ -1,5 +1,14 @@
+import entities.RegistrazioneAudio;
+
 public class Main {
     public static void main(String[] args) {
+
+        RegistrazioneAudio audio = new RegistrazioneAudio("Test Audio", 3);
+
+        audio.alzaVolume();
+        audio.alzaVolume();
+
+        audio.play();
 
     }
 }
