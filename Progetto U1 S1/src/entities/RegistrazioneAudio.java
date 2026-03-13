5bf1610a02ba9fa42b358a46075149cc584e6a5f
Fabrizio Pesaresi <fabryzio2391@gmail.com>
2026-03-13 12:56:55 +0100

    impl. interface

--- Progetto U1 S1/src/entities/RegistrazioneAudio.java
+++ Progetto U1 S1/src/entities/RegistrazioneAudio.java
@@ -1,6 +1,7 @@
 package entities;
+import interfaces.Riproducibile;
 
-public class RegistrazioneAudio extends ElementoMultimediale {
+public class RegistrazioneAudio extends ElementoMultimediale implements Riproducibile {
     private int volume;
     private int durata;
 
@@ -13,4 +14,14 @@ public class RegistrazioneAudio extends ElementoMultimediale {
     public void alzaVolume() { volume++; }
     public void abbassaVolume() { if(volume > 0) volume--; }
 
+    @Override
+    public void play() {
+        for(int i = 0; i < durata; i++) {
+            System.out.print(titolo);
+            for(int j = 0; j < volume; j++) {
+                System.out.print("!");
+            }
+            System.out.println();
+        }
     }
+}
